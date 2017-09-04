6614bd7c52bc985dd1c711665aa3f7c33664755c
Brian McMinn <brianmsj@gmail.com>
2017-09-03 17:26:34 -0700

    Update service.js

--- service.js
+++ service.js
@@ -3,6 +3,7 @@ function onChange(control, oldValue, newValue, isLoading, isTemplate) {
       return;
 
    var maxDuration = g_form.getIntValue('max_duration');
+   var minDuration = g_form.getIntValue('min duration')
 
    if (maxDuration < 5 || maxDuration > 30 * 24 * 60 * 60) {
       g_form.showFieldMsg('max_duration', getMessage('The maximum duration must be between 5 and 2,592,000 seconds (30 days)'), 'error');
